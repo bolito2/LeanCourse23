ad85812d085b5021f7456c1a6d1c64791f961544
bolito2 <bolito2hd@gmail.com>
2023-11-02 14:57:23 +0100

    wario time

--- LeanCourse/MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ LeanCourse/MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,10 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  apply lt_trans h₁
+  exact lt_of_le_of_lt h₂ h₃
+
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -86,22 +89,40 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  have h1 : a + d ≤ a + e := by
+    apply add_le_add_left h₀
+  apply add_le_add_left
+  apply exp_le_exp.2
+  exact h1
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
-  have h₁ : 0 < 1 + exp b := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    rw [← add_zero 0]
+    apply add_lt_add
+    · norm_num
+    apply exp_pos
+
+  have h₁ : 0 < 1 + exp b := by
+    rw [← add_zero 0]
+    apply add_lt_add
+    · norm_num
+    apply exp_pos
+
   apply (log_le_log h₀ h₁).mpr
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.2
+  exact h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  have h1 : exp a ≤ exp b := exp_le_exp.2 h
+  linarith
 
 example : 2 * a * b ≤ a ^ 2 + b ^ 2 := by
   have h : 0 ≤ a ^ 2 - 2 * a * b + b ^ 2
@@ -124,6 +145,21 @@ example : 2 * a * b ≤ a ^ 2 + b ^ 2 := by
   linarith
 
 example : |a * b| ≤ (a ^ 2 + b ^ 2) / 2 := by
-  sorry
+  have h1 : a * b ≤ (a ^ 2 + b ^ 2) / 2 := by
+    have h : 0 ≤ a ^ 2 - 2 * a * b + b ^ 2
+    calc
+      a ^ 2 - 2 * a * b + b ^ 2 = (a - b) ^ 2 := by ring
+      _ ≥ 0 := by apply pow_two_nonneg
+    linarith
+  have h2 : -(a * b) ≤ (a ^ 2 + b ^ 2) / 2 := by
+    have h : 0 ≤ a ^ 2 + 2 * a * b + b ^ 2
+    calc
+      a ^ 2 + 2 * a * b + b ^ 2 = (a + b) ^ 2 := by ring
+      _ ≥ 0 := by apply pow_two_nonneg
+    linarith
+  apply abs_le'.mpr
+  constructor
+  · apply h1
+  apply h2
 
 #check abs_le'.mpr
--- LeanCourse/MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ LeanCourse/MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -38,19 +38,78 @@ example : min a b = min b a := by
     apply min_le_right
     apply min_le_left
 
+#check (max_le)
+
 example : max a b = max b a := by
-  sorry
+  have h : ∀ x y : ℝ, max x y ≤ max y x := by
+    intro x y
+    apply max_le
+    · apply le_max_right
+    apply le_max_left
+
+  apply le_antisymm
+  apply h
+  apply h
+
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+  show min (min a b) c ≤ min a (min b c)
+  apply le_min
+  have h1 : min a b ≤ a := by apply min_le_left
+  have h2 : min (min a b) c ≤ min a b := by apply min_le_left
+
+  apply le_trans h2 h1
+  apply le_min
+  have h : min a b ≤ b := by apply min_le_right
+  have h' : min (min a b) c ≤ min a b := by apply min_le_left
+  apply le_trans h' h
+  apply min_le_right
+
+  apply le_min
+  · apply le_min
+    · apply min_le_left
+    · apply le_trans
+      apply min_le_right
+      apply min_le_left
+  apply le_trans
+  · apply min_le_right
+  apply min_le_right
+
+
+
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  apply add_le_add_right
+  apply min_le_left
+  apply add_le_add_right
+  apply min_le_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  · apply aux a b c
+
+  have h : min (a + c) (b + c) + (-c) ≤ min (a + c + -c) (b + c + -c) := by apply aux
+  have h1 : min (a + c - c) (b + c - c) = min a b := by
+    rw[add_sub_cancel, add_sub_cancel]
+
+  have h2 : min (a + c) (b + c) + (-c) ≤ min a b := by
+    apply le_trans
+    · apply h
+    apply le_of_eq
+    apply h1
+
+  linarith
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
-end
+example : |a| - |b| ≤ |a - b| := by
+  have h : |a| ≤ |a - b| + |b| := by
+    nth_rewrite 1 [← sub_add_cancel a b]
+    apply abs_add
+  linarith
+
 
 section
 variable (w x y z : ℕ)
@@ -66,7 +125,13 @@ example : x ∣ x ^ 2 := by
    apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  · apply dvd_add
+    · apply dvd_mul_of_dvd_right
+      apply dvd_mul_right
+    apply dvd_mul_left
+  apply dvd_mul_of_dvd_right
+  apply h
 end
 
 section
@@ -77,6 +142,10 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_right n : Nat.lcm n 0 = 0)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
-example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+example : gcd m n = gcd n m := by
+  apply dvd_antisymm
+  repeat
+  · apply dvd_gcd
+    apply gcd_dvd_right
+    apply gcd_dvd_left
 end
