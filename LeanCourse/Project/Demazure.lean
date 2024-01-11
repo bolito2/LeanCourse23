c79aadbeabe1d5e817148723683a52151d15f976
bolito2 <bolito2hd@gmail.com>
2024-01-11 18:18:07 +0100

    it doesn't make sense to pass through the fraction field

--- LeanCourse/Project/Demazure.lean
+++ LeanCourse/Project/Demazure.lean
@@ -20,6 +20,8 @@ example : circleEquation = SwapVariables circleEquation 0 1 := by
   simp [circleEquation, SwapVariables, Transposition]
   ring
 
+#check IsLocalization.mk' (FractionRing (MvPolynomial (Fin n) ℂ))
+
 noncomputable def Demazure (p : MvPolynomial (Fin n) ℂ) (i : Fin n) (h : i < n - 1) : FractionRing (MvPolynomial (Fin n) ℂ)  :=
   have : i < n := by
     have : n - 1 < n := by
@@ -35,5 +37,17 @@ noncomputable def Demazure (p : MvPolynomial (Fin n) ℂ) (i : Fin n) (h : i < n
 
   let i'_plus_1 : Fin n := ⟨i + 1, h'⟩
   let numerator := (p - SwapVariables p i' i'_plus_1)
-  let denominator : MvPolynomial (Fin n) ℂ := (X i' - X i'_plus_1)
+  let denominator : (MvPolynomial (Fin n) ℂ) := (X i' - X i'_plus_1)
+
+  let numerator_in_frac := algebraMap (MvPolynomial (Fin n) ℂ) (FractionRing (MvPolynomial (Fin n) ℂ)) numerator
+  let denominator_in_frac := algebraMap (MvPolynomial (Fin n) ℂ) (FractionRing (MvPolynomial (Fin n) ℂ)) denominator
+
+  numerator_in_frac / denominator_in_frac
+
+lemma wario :  2 > 0 := by norm_num
+lemma wah :  0 < 2 - 1 := by norm_num
+
+noncomputable def waluigi : FractionRing (MvPolynomial (Fin 2) ℂ ) := Demazure wario circleEquation 0 wah
+example : waluigi = 0 := by
+  simp [waluigi, Demazure, circleEquation, SwapVariables, Transposition]
   sorry
