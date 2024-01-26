3e21ef34e7c608f34102415a95e298df2f68edb1
bolito2 <bolito2hd@gmail.com>
2024-01-26 11:54:01 +0100

    proved the composition relation for adjacent indices

--- LeanCourse/Project/Demazure.lean
+++ LeanCourse/Project/Demazure.lean
@@ -476,6 +476,19 @@ def Demazure (i : Fin n) : PolyFraction n →  PolyFraction n := fun p =>
     ⟩
 
 @[simp]
+lemma transposition_first {i j : Fin (n + 1)} : TranspositionFun i j i = j := by
+  simp [TranspositionFun]
+
+@[simp]
+lemma transposition_second {i j : Fin (n + 1)} : TranspositionFun i j j = i := by
+  simp [TranspositionFun]
+
+@[simp]
+lemma transposition_none {i j k : Fin (n + 1)} (h1 : k ≠ i) (h2 : k ≠ j) :
+TranspositionFun i j k = k := by
+  simp [TranspositionFun, h1, h2]
+
+@[simp]
 lemma swap_variables_first {i j : Fin (n + 1)} : SwapVariablesFun i j (X i) = X j := by
   simp [SwapVariables, SwapVariablesFun, Transposition, TranspositionFun]
 
@@ -522,14 +535,53 @@ lemma i_ne_i_plus_1 {i : ℕ} {h : i < n + 1}  {h' : i + 1 < n + 1}  :
 lemma omg {i : ℕ} : i + 1 + 1 = i + 2 := by
   ring
 
+lemma transposition_commutes_adjacent {i : Fin n} {j : Fin (n + 1)} (h0 : i < n + 1) (h1 : i + 1 < n + 1) (h2 : i + 2 < n + 1) :
+  TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ j)) =
+    TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ j)) := by
+  by_cases c0 : j = ⟨i, h0⟩
+  simp[c0]
+  by_cases c1 : j = ⟨i + 1, h1⟩
+  simp[c1]
+  by_cases c2 : j = ⟨i + 2, h2⟩
+  simp[c2]
+
+  simp[c0,c1,c2]
+
+
+lemma swap_variables_commutes_adjacent {i : Fin n} {p : MvPolynomial (Fin (n + 1)) ℂ} (h0 : i < n + 1) (h1 : i + 1 < n + 1) (h2 : i + 2 < n + 1) :
+  SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ p)) =
+    SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ p)) := by
+  simp[SwapVariablesFun, Transposition, Function.comp]
+
+  have huh : (fun x ↦
+        TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 }
+          (TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 }
+            (TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 } x))) =
+
+          (fun x ↦
+        TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 }
+          (TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 }
+            (TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 } x))) := by
+
+            funext j
+            rw[transposition_commutes_adjacent h0 h1 h2]
+  rw[huh]
+
+
+
+
 lemma composition_adjacent (i : Fin n) (h : i + 1 < n) : ∀ p : MvPolynomial (Fin (n + 1)) ℂ,
   equals ((Demazure i ∘ Demazure ⟨i+1, h⟩ ∘ Demazure i) (of_polynomial p)) ((Demazure ⟨i+1, h⟩ ∘ Demazure i ∘ Demazure ⟨i+1, h⟩) (of_polynomial p)) := by
   intro p
   simp[h, Fin.castSucc, Fin.succ, Fin.castAdd, Fin.castLE]
   norm_num
 
-  --have h1 {in1 : i < n + 1} {in2 : i + 1 < n + 1} : ({ val := i, isLt := in1 } : Fin (n + 1)) ≠ { val := i + 1, isLt := in2 } := i_ne_i_plus_1
-  have h2 {in1 : i < n + 1} {in2 : i + 1 + 1 < n + 1} : ({ val := i, isLt := in1 } : Fin (n + 1)) ≠ { val := i + 1 + 1, isLt := in2 } := by
-    rw[wario_number_one]
+  have h0 : i < n + 1 := by
     linarith
+  have h1 : i + 1 < n + 1 := by
+    linarith
+  have h2 : i + 2 < n + 1 := by
+    linarith
+
+  simp [swap_variables_commutes_adjacent h0 h1 h2]
   ring
