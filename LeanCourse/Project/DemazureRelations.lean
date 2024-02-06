1f81f043dfe881f4707f849f5f78a9e91f569533
bolito2 <bolito2hd@gmail.com>
2024-02-06 15:02:43 +0100

    moved relations and associated lemmas to another file

--- LeanCourse/Project/DemazureRelations.lean
+++ LeanCourse/Project/DemazureRelations.lean
@@ -9,42 +9,45 @@ variable {n : ℕ} (n_pos : n > 0)
 
 /- Basic composition relations for Demazure operators -/
 
-lemma composition_non_adjacent (i j : Fin n) (h : |i - j| > Fin.ofNat' 1 n_pos) :
-  Demazure i ∘ Demazure j = Demazure j ∘ Demazure i := by
-  sorry
-
-lemma composition_adjacent (i : Fin n) (h : i + 1 < n) :
-  Demazure i ∘ Demazure ⟨i+1, h⟩ ∘ Demazure i = Demazure ⟨i+1, h⟩ ∘ Demazure i ∘ Demazure ⟨i+1, h⟩ := by
-  sorry
-
-/- Lemmas with symmetric polynomials -/
-
 lemma symm_invariant_swap_variables {i j : Fin n} {g : MvPolynomial (Fin n) ℂ} (h : MvPolynomial.IsSymmetric g) :
   SwapVariables i j g = g := by
   simp[SwapVariables, SwapVariablesFun]
   exact h (Transposition i j)
 
-lemma numerator_mul_symm (i : Fin n) (f g : MvPolynomial (Fin (n + 1)) ℂ) (h : MvPolynomial.IsSymmetric g) :
-  DemazureNumerator i (g * f) = (finSuccEquiv ℂ n) g * DemazureNumerator i f := by
-  rw[unfold_demazure_numerator]
-  rw[unfold_demazure_numerator]
+lemma transposition_commutes_adjacent {i : Fin n} {j : Fin (n + 1)} (h0 : i < n + 1) (h1 : i + 1 < n + 1) (h2 : i + 2 < n + 1) :
+  TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ j)) =
+    TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ j)) := by
+  by_cases c0 : j = ⟨i, h0⟩
+  simp[c0]
+  by_cases c1 : j = ⟨i + 1, h1⟩
+  simp[c1]
+  by_cases c2 : j = ⟨i + 2, h2⟩
+  simp[c2]
+
+  simp[c0,c1,c2]
 
 
+lemma swap_variables_commutes_adjacent {i : Fin n} {p : MvPolynomial (Fin (n + 1)) ℂ} (h0 : i < n + 1) (h1 : i + 1 < n + 1) (h2 : i + 2 < n + 1) :
+  SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ p)) =
+    SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ p)) := by
+  simp[SwapVariablesFun, Transposition, Function.comp]
 
-lemma mul_symm (i : Fin n) (f g : MvPolynomial (Fin (n + 1)) ℂ) (h : MvPolynomial.IsSymmetric g) :
-  Demazure i (g * f) = g * Demazure i f := by
-  simp[Demazure, DemazureFun]
-  rw[← symm_invariant_swap_variables (Fin.castSucc i) 0 g h]
-  simp[SwapVariables]
-  rw[← swap_variables_mul (Fin.castSucc i) 0]
-  apply congrArg
+  have huh : (fun x ↦
+        TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 }
+          (TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 }
+            (TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 } x))) =
 
-  sorry
+          (fun x ↦
+        TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 }
+          (TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 }
+            (TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 } x))) := by
 
-
+            funext j
+            rw[transposition_commutes_adjacent h0 h1 h2]
+  rw[huh]
 
 lemma composition_adjacent (i : Fin n) (h : i + 1 < n) : ∀ p : MvPolynomial (Fin (n + 1)) ℂ,
-  equals ((Demazure i ∘ Demazure ⟨i+1, h⟩ ∘ Demazure i) (of_polynomial p)) ((Demazure ⟨i+1, h⟩ ∘ Demazure i ∘ Demazure ⟨i+1, h⟩) (of_polynomial p)) := by
+  equals ((Dem i ∘ Dem ⟨i+1, h⟩ ∘ Dem i) (of_polynomial p)) ((Dem ⟨i+1, h⟩ ∘ Dem i ∘ Dem ⟨i+1, h⟩) (of_polynomial p)) := by
   intro p
   simp[h, Fin.castSucc, Fin.succ, Fin.castAdd, Fin.castLE]
   norm_num
@@ -57,4 +60,97 @@ lemma composition_adjacent (i : Fin n) (h : i + 1 < n) : ∀ p : MvPolynomial (F
     linarith
 
   simp [swap_variables_commutes_adjacent h0 h1 h2]
+  ring
+
+lemma unfold_non_adjacent (i j : Fin n) (h : |(i.val : ℤ ) - j.val| > 1) :
+  (Fin.castSucc i : Fin (n + 1)) ≠ (Fin.castSucc j : Fin (n + 1)) ∧
+  (Fin.castSucc i : Fin (n + 1)) ≠ (Fin.succ j : Fin (n + 1)) ∧
+  (Fin.succ i : Fin (n + 1)) ≠ (Fin.castSucc j : Fin (n + 1)) ∧
+  (Fin.succ i : Fin (n + 1)) ≠ (Fin.succ j : Fin (n + 1)) := by
+    constructor
+    · apply Fin.val_ne_iff.mp
+      simp [Fin.castSucc]
+      intro wah
+      simp[wah] at h
+    constructor
+    · apply Fin.val_ne_iff.mp
+      simp [Fin.castSucc]
+      intro wah
+      simp[wah] at h
+    constructor
+    · apply Fin.val_ne_iff.mp
+      simp [Fin.castSucc]
+      intro wah
+      simp[← wah] at h
+    apply Fin.val_ne_iff.mp
+    simp [Fin.castSucc]
+    intro wah
+    simp[wah] at h
+
+lemma transposition_commutes_non_adjacent (i j : Fin n) {k : Fin (n + 1)} (h : |(i.val : ℤ ) - j.val| > 1) :
+  TranspositionFun (Fin.castSucc i) (Fin.succ i) (TranspositionFun (Fin.castSucc j) (Fin.succ j) k) =
+   TranspositionFun (Fin.castSucc j) (Fin.succ j) (TranspositionFun (Fin.castSucc i) (Fin.succ i) k) := by
+    rcases unfold_non_adjacent i j h with ⟨h1, h2, h3, h4⟩
+
+    by_cases c0 : k = Fin.castSucc i
+    simp[h1,h2,h3,h4,c0]
+    simp[transposition_none h3 h4]
+
+    by_cases c1 : k = Fin.succ i
+    simp[h1,h2,h3,h4,c1]
+    simp[transposition_none h3 h4]
+
+    by_cases c2 : k = Fin.castSucc j
+    simp[h1,h2,h3,h4,c2]
+    simp[transposition_none h2.symm h4.symm]
+    simp[transposition_none h1.symm h3.symm]
+
+    by_cases c3 : k = Fin.succ j
+    simp[transposition_none h1.symm h3.symm, c3]
+    simp[transposition_none h2.symm h4.symm]
+
+    simp[h1,h2,h3,h4,c0,c1,c2,c3]
+
+lemma swap_variables_commutes_non_adjacent (i j : Fin n) (h : |(i.val : ℤ ) - j.val| > 1)
+ {p : MvPolynomial (Fin (n + 1)) ℂ} :
+  SwapVariablesFun (Fin.castSucc i) (Fin.succ i) (SwapVariablesFun (Fin.castSucc j) (Fin.succ j) p) =
+   SwapVariablesFun (Fin.castSucc j) (Fin.succ j) (SwapVariablesFun (Fin.castSucc i) (Fin.succ i) p) := by
+    simp[SwapVariablesFun, Transposition, Function.comp]
+
+    have wah : (fun x ↦ TranspositionFun (Fin.castSucc i) (Fin.succ i) (TranspositionFun (Fin.castSucc j) (Fin.succ j) x)) =
+    (fun x ↦ TranspositionFun (Fin.castSucc j) (Fin.succ j) (TranspositionFun (Fin.castSucc i) (Fin.succ i) x)) :=
+    by
+      funext k
+      rw[transposition_commutes_non_adjacent i j h]
+    rw[wah]
+
+
+lemma composition_non_adjacent (i j : Fin n)  (h : |(i.val : ℤ ) - j.val| > 1) : ∀ p : MvPolynomial (Fin (n + 1)) ℂ,
+  equals ((Dem i ∘ Dem j) (of_polynomial p)) ((Dem j ∘ Dem i) (of_polynomial p)) := by
+  intro p
+  simp[equals]
+
+  rcases unfold_non_adjacent i j h with ⟨h1, h2, h3, h4⟩
+
+  simp[swap_variables_commutes_non_adjacent i j h, h1, h2, h3, h4, h1.symm, h2.symm, h3.symm, h4.symm]
+
+  rw[swap_variables_none']
+  rw[swap_variables_none']
+
+  ring
+
+  exact h3
+  exact h4
+  exact h2.symm
+  exact h4.symm
+
+/-- Composition with multiplication by monomial -/
+lemma composition_mul_monomial_non_adjacent (i j : Fin n) (h : |(i.val : ℤ ) - j.val| > 1) : ∀ p : MvPolynomial (Fin (n + 1)) ℂ,
+  equals (Dem i (of_polynomial (p * X (Fin.castSucc j)))) (mul (Dem i (of_polynomial p)) (of_polynomial (X (Fin.castSucc j)))) := by
+  intro p
+  simp[equals, mul]
+
+  rcases unfold_non_adjacent i j h with ⟨h1, h2, h3, h4⟩
+  left
+  simp[swap_variables_commutes_non_adjacent i j h, h1, h2, h3, h4, h1.symm, h2.symm, h3.symm, h4.symm]
   ring
