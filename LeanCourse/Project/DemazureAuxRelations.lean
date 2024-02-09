880f76cb4e1a91e6a4cf124acbd90939b7ab7512
bolito2 <bolito2hd@gmail.com>
2024-02-09 23:10:02 +0100

    commented aux relations

--- LeanCourse/Project/DemazureAuxRelations.lean
+++ LeanCourse/Project/DemazureAuxRelations.lean
@@ -26,57 +26,6 @@ lemma demaux_order_two : ∀ (i : Fin n) (p : PolyFraction n),
   simp[r, DemAux']
   ring
 
--- Prove the relation between demazure operations with adjacent indices
-lemma transposition_commutes_adjacent {i : Fin n} {j : Fin (n + 1)} (h0 : i < n + 1) (h1 : i + 1 < n + 1) (h2 : i + 2 < n + 1) :
-  TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ j)) =
-    TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ j)) := by
-  by_cases c0 : j = ⟨i, h0⟩
-  simp[c0]
-  by_cases c1 : j = ⟨i + 1, h1⟩
-  simp[c1]
-  by_cases c2 : j = ⟨i + 2, h2⟩
-  simp[c2]
-
-  simp[c0,c1,c2]
-
-lemma swap_variables_commutes_adjacent {i : Fin n} {p : MvPolynomial (Fin (n + 1)) ℂ} (h0 : i < n + 1) (h1 : i + 1 < n + 1) (h2 : i + 2 < n + 1) :
-  SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ p)) =
-    SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ p)) := by
-  simp[SwapVariablesFun, Transposition, Function.comp]
-
-  have huh : (fun x ↦
-        TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 }
-          (TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 }
-            (TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 } x))) =
-
-          (fun x ↦
-        TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 }
-          (TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 }
-            (TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 } x))) := by
-
-            funext j
-            rw[transposition_commutes_adjacent h0 h1 h2]
-  rw[huh]
-
-lemma demaux_commutes_adjacent (i : Fin n) (h : i + 1 < n) : ∀ p : MvPolynomial (Fin (n + 1)) ℂ,
-  (DemAux i ∘ DemAux ⟨i+1, h⟩ ∘ DemAux i) (mk' p) = (DemAux ⟨i+1, h⟩ ∘ DemAux i ∘ DemAux ⟨i+1, h⟩) (mk' p) := by
-  intro p
-  simp[DemAux, mk']
-  repeat rw[lift_r]
-  simp[DemAux']
-  simp[h, Fin.castSucc, Fin.succ, Fin.castAdd, Fin.castLE]
-  norm_num
-
-  have h0 : i < n + 1 := by
-    linarith
-  have h1 : i + 1 < n + 1 := by
-    linarith
-  have h2 : i + 2 < n + 1 := by
-    linarith
-
-  simp [swap_variables_commutes_adjacent h0 h1 h2]
-  ring
-
 -- Helper lemma to get all important inequalities for non-adjacent indices
 lemma unfold_non_adjacent (i j : Fin n) (h : |(i.val : ℤ ) - j.val| > 1) :
   (Fin.castSucc i : Fin (n + 1)) ≠ (Fin.castSucc j : Fin (n + 1)) ∧
@@ -213,3 +162,56 @@ lemma DemAux_mul_symm (i : Fin n) (g f : PolyFraction n) (h : IsSymmetric g) :
 
   simp[symm_invariant_swap_variables g_num_symm, symm_invariant_swap_variables g_denom_symm]
   ring
+
+
+-- Prove the relation between demazure operations with adjacent indices
+lemma transposition_commutes_adjacent {i : Fin n} {j : Fin (n + 1)} (h0 : i < n + 1) (h1 : i + 1 < n + 1) (h2 : i + 2 < n + 1) :
+  TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ j)) =
+    TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (TranspositionFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (TranspositionFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ j)) := by
+  by_cases c0 : j = ⟨i, h0⟩
+  simp[c0]
+  by_cases c1 : j = ⟨i + 1, h1⟩
+  simp[c1]
+  by_cases c2 : j = ⟨i + 2, h2⟩
+  simp[c2]
+
+  simp[c0,c1,c2]
+
+lemma swap_variables_commutes_adjacent {i : Fin n} {p : MvPolynomial (Fin (n + 1)) ℂ} (h0 : i < n + 1) (h1 : i + 1 < n + 1) (h2 : i + 2 < n + 1) :
+  SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ p)) =
+    SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ (SwapVariablesFun ⟨i, h0⟩ ⟨i + 1, h1⟩ (SwapVariablesFun ⟨i + 1, h1⟩ ⟨i + 2, h2⟩ p)) := by
+  simp[SwapVariablesFun, Transposition, Function.comp]
+
+  have huh : (fun x ↦
+        TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 }
+          (TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 }
+            (TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 } x))) =
+
+          (fun x ↦
+        TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 }
+          (TranspositionFun { val := i, isLt := h0 } { val := i + 1, isLt := h1 }
+            (TranspositionFun { val := i + 1, isLt := h1 } { val := i + 2, isLt := h2 } x))) := by
+
+            funext j
+            rw[transposition_commutes_adjacent h0 h1 h2]
+  rw[huh]
+
+-- This takes a lot of time to compute
+lemma demaux_commutes_adjacent (i : Fin n) (h : i + 1 < n) : ∀ p : MvPolynomial (Fin (n + 1)) ℂ,
+  (DemAux i ∘ DemAux ⟨i+1, h⟩ ∘ DemAux i) (mk' p) = (DemAux ⟨i+1, h⟩ ∘ DemAux i ∘ DemAux ⟨i+1, h⟩) (mk' p) := by
+  intro p
+  simp[DemAux, mk']
+  repeat rw[lift_r]
+  simp[DemAux']
+  simp[h, Fin.castSucc, Fin.succ, Fin.castAdd, Fin.castLE]
+  norm_num
+
+  have h0 : i < n + 1 := by
+    linarith
+  have h1 : i + 1 < n + 1 := by
+    linarith
+  have h2 : i + 2 < n + 1 := by
+    linarith
+
+  simp [swap_variables_commutes_adjacent h0 h1 h2]
+  sorry
